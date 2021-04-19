9ac578ebcab77a2db0ba89a2bc5d578ca214c73f
zazazhao <zaza@txzing.com>
2021-04-19 15:50:38 +0800

    Initial commit

---
+++ app/src/main/java/com/txznet/dpidemo/MainActivity.java
@@ -0,0 +1,80 @@
+package com.txznet.dpidemo;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //去除标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //去除状态栏
+        //SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        WindowManager windowManager = getWindow().getWindowManager();
+
+
+        TextView tvRealSize=(TextView) findViewById(R.id.tv_real_size);
+        Point point_fact = new Point();
+        tvRealSize.setText("width:"+point_fact.x+"\n" +
+                "height:"+point_fact.y);
+
+
+        TextView tvSize=(TextView) findViewById(R.id.tv_size);
+        Point point_use = new Point();
+        windowManager.getDefaultDisplay().getSize(point_use);
+        tvSize.setText("width:"+point_use.x+"\n" +
+                "height:"+point_use.y);
+
+        TextView tvRect=(TextView) findViewById(R.id.tv_rect);
+        Rect rect=new Rect();
+        windowManager.getDefaultDisplay().getRectSize(rect);
+        tvRect.setText("top:"+rect.top+"\n" +
+                "bottom:"+rect.bottom+"\n"+
+                "left:"+rect.left+"\n"+
+                "right:"+rect.right);
+
+
+        TextView tvMetrics=(TextView) findViewById(R.id.tv_metrics);
+        DisplayMetrics metrics=new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(metrics);
+        tvMetrics.setText("width:"+metrics.widthPixels+"\n" +
+                "height:"+metrics.heightPixels);
+
+        TextView tvRealMetrics=(TextView) findViewById(R.id.tv_real_metrics);
+        DisplayMetrics metrics1=new DisplayMetrics();
+        windowManager.getDefaultDisplay().getRealMetrics(metrics1);
+        tvRealMetrics.setText("width:"+metrics1.widthPixels+"\n" +
+                "height:"+metrics1.heightPixels);
+
+        Button btnUi1=(Button) findViewById(R.id.btn_ui_1);
+        btnUi1.setOnClickListener(view -> {
+            Intent intent=new Intent(this,UiTest1Activity.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/txznet/dpidemo/UiTest1Activity.java
@@ -0,0 +1,55 @@
+package com.txznet.dpidemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class UiTest1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //去除标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //去除状态栏
+        //SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+//        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+//        decorView.setSystemUiVisibility(uiOptions);
+        hideBottomUIMenu();
+
+
+        setContentView(R.layout.activity_ui_test1);
+    }
+
+    protected void hideBottomUIMenu() {
+        //隐藏虚拟按键，并且全屏
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+
+}
